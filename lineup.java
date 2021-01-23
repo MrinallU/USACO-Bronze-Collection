f6eeab041ffb317443121beebfa6832d8fbf9fc4
MrinallU <61529929+MrinallU@users.noreply.github.com>
2021-01-23 11:38:43 -0500

    Update lineup.java

--- lineup.java
+++ lineup.java
@@ -7,10 +7,10 @@ TASK: lineup
 import java.io.*;
 import java.util.*;
 public class lineup{
-    boolean check(String [][] logs, int [] arr){
+    boolean check(String [][] logs, int [] arr){ // Make sure the current permuatation has all of the cows in the correct order.
         for(int i = 0; i < logs.length; i++){
             int a = 0; int b =  0;
-            if(logs[i][0].equals("Beatrice")){
+            if(logs[i][0].equals("Beatrice")){ // Convert the cows to numbers to match the logs array
                 a = 1;
             }
             if(logs[i][0].equals("Belinda")){
@@ -67,15 +67,15 @@ public class lineup{
 
     boolean finalCheck(int a, int b,int [] arr) {
         int indexa = 0; int indexb=  0;
-        for(int i  = 0; i < arr.length; i++){
+        for(int i  = 0; i < arr.length; i++){ // Find the index postion of both of the items in the logs array.
             if(arr[i] == a){
-                indexa = i + 1;
+                indexa = i + 1; 
             }
             if(arr[i] == b){
                 indexb = i + 1;
             }
         }
-        return Math.abs(indexa - indexb) == 1;
+        return Math.abs(indexa - indexb) == 1; // Return if they are next to each other in that current permuatation meaning one of the conditions is fufilled.
     }
 
     boolean nextPermutation(int[] array) {
@@ -117,19 +117,27 @@ public class lineup{
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(new File("lineup.in"));
         PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("lineup.out")));
-        int N = sc.nextInt(); String [][] logs = new String[N][2]; String[] arr = new String [8]; int [] arr1  = new int[8];
-        for(int i = 0; i < N; i++){
+        
+        int N = sc.nextInt(); 
+        String [][] logs = new String[N][2]; 
+        String[] arr = new String [8]; 
+        int [] arr1  = new int[8];
+        
+        for(int i = 0; i < N; i++){ // Store the conditions that msut be fufilled for a valid permutation of cows.
             logs[i][0] = sc.next();
             String a = sc.next();String b = sc.next();String c = sc.next();String d = sc.next();
             logs[i][1] = sc.next();
         }
-        arr1[0] = 1;  arr1[1] =2; arr1[2] =3; arr1[3] = 4; arr1[4] = 5; arr1[5] = 6; arr1[6] = 7; arr1[7] = 8;
+        
+        arr1[0] = 1;  arr1[1] =2; arr1[2] =3; arr1[3] = 4; arr1[4] = 5; arr1[5] = 6; arr1[6] = 7; arr1[7] = 8; // Convert the cows to numebers for the next_pemutation function.
+        
         arr[0] = "Beatrice";  arr[1] ="Belinda"; arr[2] ="Bella"; arr[3] = "Bessie"; arr[4] = "Betsy"; arr[5] = "Blue"; arr[6] = "Buttercup"; arr[7] = "Sue";
-        while (new lineup().nextPermutation(arr1)){
-            if(new lineup().check(logs, arr1)) {
+        
+        while (new lineup().nextPermutation(arr1)){ 
+            if(new lineup().check(logs, arr1)) { // Use the logs array to check all the cows are in the proper order.
                 for (int i = 0; i < arr1.length; i++){
                     if(arr1[i] == 1){
-                        out.println("Beatrice");
+                        out.println("Beatrice"); 
                     }
                     if(arr1[i] == 2){
                         out.println("Belinda");
